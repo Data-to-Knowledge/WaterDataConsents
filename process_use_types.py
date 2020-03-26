4f4d408fe4da685ddb718d416c0fa62d25b96f21
Mike Kittridge <mullenkamp1@gmail.com>
2020-03-26 16:28:20 +1300

    switched accela_use from on-prem to snowflake

--- process_use_types.py
+++ process_use_types.py
@@ -4,14 +4,8 @@ Created on Fri Mar  6 16:19:11 2020
 
 @author: MichaelEK
 """
-import os
-import argparse
 import types
 from pdsql import mssql
-from datetime import datetime
-import yaml
-import itertools
-import types
 import pandas as pd
 import numpy as np
 import json
@@ -46,19 +40,19 @@ def process_use_types(param):
     permit_use1 = db.accela_use.copy()
     permit_use1['RecordNumber'] = permit_use1['RecordNumber'].str.upper().str.strip()
     permit_use1['Activity'] = permit_use1['Activity'].str.title().str.strip()
-    permit_use1['Water Use'] = permit_use1['Water Use'].str.strip()
+    permit_use1['UseType'] = permit_use1['UseType'].str.strip()
     permit_use2 = permit_use1.drop_duplicates().copy()
 #    permit_use2 = permit_use1.drop_duplicates(['RecordNumber']).drop('Activity', axis=1).copy()
 
     ## Check that all use types exist in the mapping table
     uses1 = db.use_mapping.Accela.unique()
-    uses2 = permit_use2['Water Use'].unique()
+    uses2 = permit_use2['UseType'].unique()
     mis_uses = uses2[~np.in1d(uses2, uses1)]
     if mis_uses.shape[0] > 0:
         raise ValueError('Missing some use types')
 
     ## Combine other use types
-    permit_use2.rename(columns={'Water Use': 'Accela'}, inplace=True)
+    permit_use2.rename(columns={'UseType': 'Accela'}, inplace=True)
     permit_use3 = pd.merge(permit_use2, db.use_mapping, on='Accela')
     permit_use4 = pd.merge(permit_use3, db.use_priorities, on='WaitakiTable5')
     permit_use4.sort_values('Rank', inplace=True)
